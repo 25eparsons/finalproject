7389677ada58600fb2c8d0ddff8ab64c9cd7b410
25eparsons <25eparsons@go.dsdmail.net>
2023-02-10 11:04:13 -0700

    Added introduction cabin message.

---
+++ main.py
@@ -0,0 +1,8 @@
+#intro cabin text
+import random
+time = random.randint(10,59)
+print("You wake up in a cabin bed, you don't know how you got here.")
+print(f"The clock beside you reads 10:{time} PM")
+print("You look outside the window, there is a dark forest")
+
+
