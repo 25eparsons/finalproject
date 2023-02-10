ffbc7f28f456172a873386b4f17802560a6a64dc
25eparsons <25eparsons@go.dsdmail.net>
2023-02-10 11:13:14 -0700

    Added options

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 import random
 time = random.randint(10,59)
 print("You wake up in a cabin bed, you don't know how you got here.")
-print(f"The clock beside you reads 10:{time} PM")
-print("You look outside the window, there is a dark forest")
-
-
+print(f"The clock beside you reads 10:{time} PM.")
+print("You look outside the window, there is a dark forest.")
+print("\nOptions:")
+print("Leave the cabin or go back to sleep until morning?")
+#forest text
+print("You are now in the forest")
+print("") 
