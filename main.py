d41a3f689b53c8d384095e1965b647331a454fae
25eparsons <25eparsons@go.dsdmail.net>
2023-02-15 12:25:40 -0700

    Changed some stuff

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 #intro cabin text
-import random
-time = random.randint(10,59)
-print("You wake up in a cabin bed, you don't know how you got here.")
-print(f"The clock beside you reads 10:{time} PM.")
-print("You look outside the window, there is a dark forest.")
-print("\nOptions:")
+cabinintro = "You wake up in a cabin bed, you don't know how you got here.\nThe clock beside you reads 10:30 PM.\nYou look outside the window, there is a dark forest.\n\nOptions:"
+print(cabinintro)
 choice = ""
 while choice != "leave":
 	choice = input("Leave the cabin or stay until morning?\n")
 	if choice == "stay":
-		time = random.randint(10,59)
 		print("You wake back up in the cabin")
-		print(f"It is now 8:{time} AM")
+		print(f"It is now 8:15 AM")
 	elif choice != "leave" or choice != "stay":
 		print("-improper input message-")
-print("-forest message-")
+
+
+#forest message
+print("You are now in the forest")
+print("Options:")
+choice = input("")
 
 print("You're back in the cabin")
 # cabin text
---
+++ text.py
@@ -0,0 +1,9 @@
+firstcabin = "You wake up in a cabin bed, you don't know how you got here.\nThe clock beside you reads 10:30 PM.\nYou look outside the window, there is a dark forest.\n\nOptions:"
+cabin = 
+
+firstforest = "You are now in the forest."
+forest = "You are in a different part of the forest"
+forestmsg1 = "You hear a loud noise.\nYou look around but don't see anything."
+forestmsg2 = "You notice how muddy the ground is."
+forestmsg3 = "You are starting to feel hungry.\nYou consider going back to the cabin to find food."
+forestmsg4 = ""
