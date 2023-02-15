791c2969869fcd86a7853714194ca12314cf2e43
25eparsons <112005058+25eparsons@users.noreply.github.com>
2023-02-15 21:33:16 +0000

    Added ladder text finished hatch

--- text.py
+++ text.py
@@ -24,13 +24,22 @@ food_leave = "You decide to leave it alone.\nYou dont want anything to do with i
 forest_guard = "You see a guard standing in front of a large stone statue.\nWhat do you choose to do?"
 guard_choice = "Approach the guard or ignore him?"
 guard_approach = "You approach the guard.\nHe says he will move if you give him food."
-guard_approach_nofood = "You do not have any food.\n You should keep looking."
+guard_approach_nofood = "You do not have any food.\nYou should keep looking."
+guard_approach_choice = "Do you give the guard food?"
 guard_approach_give = "You give the gaurd some of your food.\nHe moves aside revealing a key in the stone.\nYou pick up the key."
 guard_approach_refuse = "You refuse to give the guard your food.\nHe wishes you a good day."
 guard_ignore = "You ignore the guard and leave that part of the forest"
 
 forest_hatch = "You come across a large concrete pad with two metal doors laying on it.\nWhat do you choose to do?"
 hatch_choice = "Open the doors or come back later?"
-hatch_choice_
+hatch_open = "You try to open the doors but notice a lock preventing them form being opened."
+hatch_open_keychoice = "Do you want to use your key?"
+hatch_open_yes = "You use your key to unlock the doors\nWhen you open them, you see a ladder leading down into darkness.\nWhat do you choose to do?"
+hatch_open_no = "You save your key for a lucky day."
+hatch_open_nokey = "It looks like you need to find a key."
+
+ladder_choice = "Climb down the ladder or leave and come back later?"
+ladder_down = "You begin to climb down the ladder.\nIt is starting to get very dark."
+ladder_leave = "You leave the area of the forest with the ladder."
 
 print(first_cabin)
