9874cf5ef07547a0411feca11297fc2c1713dc90
25eparsons <112005058+25eparsons@users.noreply.github.com>
2023-02-16 00:16:59 +0000

    started on code

---
+++ main.py
@@ -0,0 +1,11 @@
+from text import *
+
+print(first_cabin)
+choice = input(fcabin_choice)
+while choice != fcabin_choice_opt[1]:
+	if choice not in fcabin_choice_opt:
+		print(f"\nInvalid command\nUse {fcabin_choice_opt}")
+	else:
+		print(fcabin_stay)
+	choice = input(fcabin_choice)
+print(fcabin_leave)
