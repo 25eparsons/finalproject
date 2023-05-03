62d8f785add9f38049729fddf9a8ac4f5dad2d85
25eparsons <25eparsons@go.dsdmail.net>
2023-05-03 13:09:43 -0600

    In the procces of reqwriting

--- newmain.py
+++ newmain.py
@@ -1,37 +1,123 @@
 import pickle
 
-COMMANDS = '''Here are your commands:
+start = '''Welcome to
+ _____  _             _____                       _   
+|_   _|| |__    ___  |  ___|___   _ __  ___  ___ | |_ 
+  | |  | '_ \  / _ \ | |_  / _ \ | '__|/ _ \/ __|| __|
+  | |  | | | ||  __/ |  _|| (_) || |  |  __/\__ \| |_ 
+  |_|  |_| |_| \___| |_|   \___/ |_|   \___||___/ \__|
+
+Here are your commands:
 quit - leave the game
 save - save the game
 load - load game from last save
 use  - use an item from you inventory
 grab - pickup an item
-move - desplay movement options'''
+move - desplay movement options
+		(Press enter to begin)\n\n'''
+
+locations = {
+"cabin": [["north", "f6"]],
+"f6": [["north", "f3"],["south", "cabin"],["east", "food"],["west", "f5"]],
+"food": [["north", "f4"],["west", "f6"]],
+"f5": [["north", "key"],["east", "f6"]],
+"key": [["north", "f1"],["south", "f5"],["east", "f3"]],
+"f3": [["north", "f2"],["south", "f6"],["east", "f4"],["west", "key"]],
+"f4": [["north", "hatch"],["south", "food"],["west", "f3"]],
+"f1": [["south", "key"],["east", "f2"]],
+"f2": [["south", "f3"],["east", "hatch"],["west", "f1"]],
+"hatch": [["south", "f4"],["west", "f2"]],
+}
+
+descriptions = {
+"cabin": "You are in a small cabin. There is a table with a map and a lamp on it.",
+"f6": "You are in a forest. There are trees all around you.",
+"food": "You found a stash of canned food. It looks like it's been here for a while.",
+"f5": "You found an abandoned campsite. There is a tent and a fire pit.",
+"key": "You found a key on the ground.",
+"f3": "You are in a clearing. There are some bushes and a pile of rocks.",
+"f4": "You are in a forest. You think you hear a machine.",
+"f1": "You are in a swamp. The ground is wet and slippery.",
+"f2": "You are in a dense forest. It's hard to see where you're going.",
+"hatch": "You found a metal hatch. There is a padlock on it."
+}
 
 class Player():
-	'''A player'''
-	def __init__(self, location = "Home"):
-		self.location = location
+	'''A player object'''
+	def __init__(self):
+		self.__location = "cabin"
+		self.inventory = []
+		name = input("What is your name?\n")
+		print(f"Good luck {name},")
+		input(start)
+		print(descriptions[self.__location])
+
+	def __str__(self):
+		pass
+
+	@property
+	def location(self):
+		return self.__location
+
+	@location.setter
+	def location(self, new):
+		self.__location = new
+
+	def movement(self, current):
+		num = 0
+		choice_list = []
+		for i in locations[current]:
+			moves = locations.get(current)[num][0]
+			choice_list.append(moves)
+			num += 1
+		move = self.check_choice(choice_list)
+		for i in locations[current]:
+			if i[0] == move:
+				new_location = i[1]
+				self.__location = new_location
+				print(descriptions[new_location])
+				return new_location
+
+	def check_choice(self, choices):
+		print('\nOptions:')
+		num = 0
+		for i in choices:
+			print(f"- {choices[num].title()}")
+			num += 1
+		choice = input('\n').lower().strip()
+		while choice not in choices:
+			print('\nInvalid command\nOptions:')
+			num = 0
+			for i in choices:
+				print(f"- {choices[num].title()}")
+				num += 1
+			choice = input('').lower().strip()
+		return choice
 
 
-def quit(save):
-	if save == False:
-		i = input("Do you want to save the game?\n")
-		if i.lower() == "yes":
-			save()
-	print("Thanks for playing!")
-
-def save():
-	i = input("Do you already have a save file?\n")
-	if i.lower() == "yes":
-		name = input("What is the name of the save file?\n")
-	else:
-		name = input("What do you want to name the save file?\n")
-	with open(name, "wb") as file:
-		pickle.dump(Player, file)
+def save(player):
+	with open("save_game.txt", "wb") as file:
+		pickle.dump(player, file)
+		print("Game Saved!")
 
 def load():
-	name = input("What is the name of the save file?\n")
-	with open(name, "rb") as file:
-		pickle.load(file)
-save()
+	with open("save_game.txt", "rb") as file:
+		player = pickle.load(file)
+		print("Game Loaded!")
+	return player
+
+def quit():
+	print("Thanks for playing!")
+
+player = Player()
+choice = None
+while choice != "quit":
+	choice = player.check_choice(["quit", "move", "save", "load"])
+	if choice == "quit":
+		quit()
+	elif choice == "move":
+		player.location = player.movement(player.location)
+	elif choice == "save":
+		save(player)
+	elif choice == "load":
+		player = load()
