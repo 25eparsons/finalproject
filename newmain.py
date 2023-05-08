a783f1665c164166fcf9c1ddd8c98e074f781ccf
25eparsons <25eparsons@go.dsdmail.net>
2023-05-08 13:16:29 -0600

    fIXED SOME STUFF

--- newmain.py
+++ newmain.py
@@ -1,3 +1,6 @@
+
+
+
 import pickle
 
 start = '''\nWelcome to
@@ -13,12 +16,12 @@ save - save the game
 load - load game from last save
 use  - use an item from you inventory
 grab - pickup an item
-move - desplay movement options
+move - display movement options
 		(Press enter to begin)\n\n'''
 
 locations = {
 "cabin": [["north", "f6"]],
-"f6": [["north", "f3"],["south", "cabin"],["east", "food"],["west", "f5"]],
+"f6": [["north", "f3"],["south", "cabin"],["east", "food"],["west", "f5"]],  
 "food": [["north", "f4"],["west", "f6"]],
 "f5": [["north", "key"],["east", "f6"]],
 "key": [["north", "f1"],["south", "f5"],["east", "f3"]],
@@ -26,7 +29,7 @@ locations = {
 "f4": [["north", "hatch"],["south", "food"],["west", "f3"]],
 "f1": [["south", "key"],["east", "f2"]],
 "f2": [["south", "f3"],["east", "hatch"],["west", "f1"]],
-"hatch": [["south", "f4"],["west", "f2"]],
+"hatch": [["south", "f4"],["west", "f2"],["down", "bunker"]],
 }
 
 descriptions = {
@@ -39,7 +42,16 @@ descriptions = {
 "f4": "You are in a forest. You think you hear a machine.",
 "f1": "You are in a swamp. The ground is wet and slippery.",
 "f2": "You are in a dense forest. It's hard to see where you're going.",
-"hatch": "You found a metal hatch. There is a padlock on it."
+"hatch": "You found a metal hatch. There is a padlock on it.",
+"bunker": "You are in the bunker"
+}
+
+items = {
+"cabin": "flashlight",
+"food": "can of food",
+"key": "key",
+"f3": "rock",
+"bunker": "trophy"
 }
 
 
@@ -52,17 +64,15 @@ class Player():
 		print(f"Good luck {name},")
 		input(start)
 		print(descriptions[self.__location])
-
-	def __str__(self):
-		pass
+		print(f"There is a {items[self.__location]} here.")
 
 	@property
 	def location(self):
 		return self.__location
 
 	@location.setter
-	def location(self, new):
-		self.__location = new
+	def location(self, new_location):
+		self.__location = new_location
 
 	def movement(self, current):
 		num = 0
@@ -94,38 +104,17 @@ class Player():
 				num += 1
 			choice = input('').lower().strip()
 		return choice
-
-items = {
-"cabin": ["flashlight"],
-"f6": [],
-"food": ["can of food"],
-"f5": [],
-"key": ["key"],
-"f3": ["rock"],
-"f4": [],
-"f1": [],
-"f2": [],
-"hatch": [],
-}
-
-class Room():
-	'''A Room Object'''
-	def __init__(self):
-		self.__items = []
-
-	def __str__(self):
-		if self.__items:
-			print(f"There is a {self.__items} here\nWould you like to pick it up")
-
-	@property
-	def items(self):
-		return self.__items
-
-	@setter.items
-	def items(self):
-		if self.__items:
+	
+	def check_item(self, current):
+		try:
+			if items[current]:
+				print(f"There is a {items[current]} here.")
+		except:
+			print("There is nothing here.")
 
 
+	def grab(self, current):
+		pass 										#I AM HERE RIGHT NOW
 
 def save(player):
 	with open("save_game.txt", "wb") as file:
@@ -144,12 +133,15 @@ def quit():
 player = Player()
 choice = None
 while choice != "quit":
-	choice = player.check_choice(["quit", "move", "save", "load"])
+	choice = player.check_choice(["quit", "move", "save", "load", "grab"])
 	if choice == "quit":
 		quit()
 	elif choice == "move":
 		player.location = player.movement(player.location)
+		player.check_item(player.location)
 	elif choice == "save":
 		save(player)
 	elif choice == "load":
 		player = load()
+	elif choice == "grab":
+		pass
