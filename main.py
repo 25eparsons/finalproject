ab442abc36df6f6b20d30a71dde81fa32d8bed9c
25eparsons <25eparsons@go.dsdmail.net>
2023-05-12 11:17:31 -0600

    IT WORKS!

--- main.py
+++ main.py
@@ -1,264 +1,214 @@
-import random
+import pickle
 
-#important variables
-inventory = []
-got_food = 0
-guard_approached = 0
-hatch_opened = 0
-complete = 0
-started = 0
-current = "f6"
-
-#locations dictionary
-locations = {
-"cabin": [["north", "f6"]],
-"f6": [["north", "f3"],["south", "cabin"],["east", "food"],["west", "f5"]],
-"food": [["north", "f4"],["west", "f6"]],
-"f5": [["north", "key"],["east", "f6"]],
-"key": [["north", "f1"],["south", "f5"],["east", "f3"]],
-"f3": [["north", "f2"],["south", "f6"],["east", "f4"],["west", "key"]],
-"f4": [["north", "hatch"],["south", "food"],["west", "f3"]],
-"f1": [["south", "key"],["east", "f2"]],
-"f2": [["south", "f3"],["east", "hatch"],["west", "f1"]],
-"hatch": [["south", "f4"],["west", "f2"]],
-}
-
-#function for movement
-def movement(current):
-	print("\nYou can move:")
-	num = 0
-	choice_list = []
-	for i in locations[current]:
-		moves = locations.get(current)[num][0]
-		choice_list.append(moves)
-		print("- " + moves)
-		num += 1
-	print()
-	move = check(choice_list)
-	for i in locations[current]:
-		if i[0] == move:
-			return i[1]
-
-#function to show inventory
-def show_inventory():
-	print("Your inventory contains:")
-	for i in inventory:
-		print(f"- {i.title()}")
-
-#function to check choices against the possible choices
-def check_choice(choices, text):
-	print(f'{text}Options:')
-	num = 0
-	for i in choices:
-		print(f"- {choices[num]}")
-		num += 1
-	choice = input('\n').lower().strip()
-	while choice not in choices:
-		print('\nInvalid command\nOptions:')
-		num = 0
-		for i in choices:
-			print(f"- {choices[num]}")
-			num += 1
-		choice = input('').lower().strip()
-	return choice
-
-def check(choices):
-	choice = input("What is your choice?\n").lower().strip()
-	while choice not in choices:
-		print('\nInvalid command\nOptions:')
-		num = 0
-		for i in choices:
-			print(f"- {choices[num]}")
-			num += 1
-		choice = input("What is your choice?\n").lower().strip()
-	return choice
-
-#Scenario for first time in cabin.
-def cabin(started):
-	if started == 0:
-		print("You wake up in a cabin bed, you don't know how you got here.\nThe clock beside you reads 10:30 PM.\nYou look outside the window, there is a dark forest.\nYou can exit to the north.")
-		choice = check_choice(["north", "stay"], "\nWhat do you want to do?\n")
-		if choice == "north":
-			print("\nYou go north and leave the cabin\nYou have entered the forest")
-			started = 1
-		else:
-			print("\nYou stay in bed and wake up the next morning.\n")
-	else:
-		print("\nYou are back at the cabin.")
-
-#scenario for forest.
-def forest_scenario():
-	print()
-	scenario = random.randint(1, 3)
-	if scenario == 1:
-		print("You hear a loud noise.\nYou look around but don't see anything.")
-	elif scenario == 2:
-		print("You notice how muddy the ground is.")
-	elif scenario == 3:
-		print("You are starting to feel hungry and consider going back to the cabin to find food.")
-	else:
-		print("You are in a different part of the forest")
-
-
-#scenario for box of food.
-def food(got_food):
-	if got_food == 0:
-		print("\nYou find a box in the forest.")
-		choice = check_choice(["yes", "no"], "\nDo you want to open it?\n")
-		if choice == "yes":
-			print("\nYou open the box and find some food.")
-			inventory.append("Food")
-			show_inventory()
-			got_food = 1
-			return got_food
-		else:
-			print("\nYou decide not to open the box.")
-	else:
-		print("\nYou are back at the box where you found food")
-
-#scenario for guard interaction.
-def guard(guard_approached):
-	if guard_approached == 0:
-		print("\nYou see a guard standing in front of a large stone statue.")
-		choice = check_choice(["yes", "no"], "\nDo you want to talk to the guard?\n")
-		if choice == "yes":
-			print("\nYou approach the guard.\nHe says he will move if you give him your food.")
-			if "Food" in inventory:
-				choice = check_choice(["yes", "no"], "\nDo you give the guard your food?\n")
-				if choice == "yes":
-					print("\nYou give the guard your food.\nHe disappears revealing a key in the stone.\nYou pick up the key.")
-					inventory.remove("Food")
-					inventory.append("Key")
-					show_inventory()
-					guard_approached = 1
-					return guard_approached
-				else:
-					print("You refuse to give the guard your food.\nHe wishes you a good day.")
-			else:
-				print("You do not have any food.\nYou should keep looking.")
-		else:
-			print("You ignore the guard and leave that part of the forest.")
-	else:
-		print("\nYou are back where you found the guard")
-	
-#scenario for hatch
-def hatch(hatch_opened):	
-	if hatch_opened == 0:
-		print("\nYou come across a large concrete pad with two metal doors laying on it.")
-		choice = check_choice(["yes", "no"], "\nDo you try to open the doors?\n")
-		if choice == "yes":
-			if "Key" in inventory:
-				print("\nYou try to open the doors but notice a lock preventing them from being opened.")
-				choice = check_choice(["yes", "no"], "\nDo you want to use your key?\n")
-				if choice == "yes":
-					print("\nYou use your key to unlock the doors\nWhen you open them, you see a ladder leading down into darkness.")
-					inventory.remove("Key")
-					hatch_opened = 1
-					return hatch_opened
-				else:
-					print("\nYou save your key for a lucky day.")
-			else:
-				print("\nIt looks like you need to find a key.")
-		else:
-			print("\nYou don't try to open the doors and you leave that part of the forest")
-	else:
-		print("You are back at the hatch.")
-
-#Start Of Game
-
-#print out the instructions
-input('''Welcome to my game!
-Here are the instructions:
-
-** Use fullscreen terminal.
-
-** Use commands to complete objectives in 
-order to defeat the final boss.
-
-** Only one word commands are accepted
-
-** Make sure to type commands correctly.
-
-** Have fun.
-
-** Press enter to move on.
-''')
-#print out the commands
-input('''Here are the commands:
-north - move north
-south - move south
-east - move east
-west - move west
-up - move up
-down - move down
-inventory - view your inventory
-use - use an item
-grab - pick up an item
-stay - stay where you are
-goals - view your objectives
-''')
-
-#get the player's name
-playername = input("What do you want your name to be?\n")
-print(f"\nGood luck {playername},")
-#print out the game title
-input('''Welcome to
-
+start = '''\nWelcome to
  _____  _             _____                       _   
 |_   _|| |__    ___  |  ___|___   _ __  ___  ___ | |_ 
   | |  | '_ \  / _ \ | |_  / _ \ | '__|/ _ \/ __|| __|
   | |  | | | ||  __/ |  _|| (_) || |  |  __/\__ \| |_ 
   |_|  |_| |_| \___| |_|   \___/ |_|   \___||___/ \__|
-		(Press enter to begin)\n''')
-print("\n\n\n")
 
+Here are your commands:
+quit - leave the game
+save - save the game
+load - load game from last save
+use  - use an item from you inventory
+grab - pickup an item
+move - display movement options
+		(Press enter to begin)\n\n'''
 
-print("You wake up in a cabin bed, you don't know how you got here.\nThe clock beside you reads 10:30 PM.\nYou look outside the window, there is a dark forest.\nYou can exit to the north.\n")
-choice = check_choice(["north", "stay"], "\nWhat do you want to do?\n")
-if choice == "north":
-	print("\nYou go north and leave the cabin\nYou have entered the forest")
-	current = "f6"
-else:
-	print("\nYou stay in bed and wake up the next morning.")
-	current = "cabin"
-started = 1
+locations = {
+"cabin": [["north", "forest"]],
+"forest": [["north", "clear"],["south", "cabin"],["west", "camp"]],  
+"camp": [["east", "forest"]],
+"clear": [["north", "deer"],["east", "noise"],["south", "forest"]],
+"key": [["north", "swamp"]],
+"swamp": [["south", "key"],["east", "deer"]],
+"deer": [["south", "clear"],["west", "swamp"]],
+"noise": [["south", "food"],["west", "clear"]],
+"food": [["north", "noise"],["east", "feet"]],
+"feet": [["north", "dark"],["west", "food"]],
+"dark": [["south", "feet"],["north", "dog"]],
+"dog": [["south", "dark"],["west", "hatch"]],
+"hatch": [["west", "dog"]],
+}
 
+descriptions = {
+"cabin": "You are in a small cabin. There is a table with a map and a flashlight on it.",
+"forest": "You are in a forest. There are trees all around you.",
+"camp": "You found an abandoned campsite. There is a tent and a fire pit.",
+"clear": "You are in a clearing. There are some bushes and a pile of rocks.",
+"key": "You see a key inside a small cave. You need to find some way to get it.",
+"swamp": "You are in a swamp. The ground is wet and slippery.",
+"deer": "You found a watering hole. There are some deer nearby.",
+"noise": "You are in a forest. You think you hear a machine.",
+"food": "You found a stash of canned food. It looks like it's been here for a while.",
+"feet": "You hear some footsteps. There might be someone following you.",
+"dark": "You are in a dense forest. It's so dark it's hard to see where you're going.",
+"dog": "You found an old house. Before you go inside a dog comes out.",
+"hatch": "You found a metal hatch. There is a padlock on it.",
+}
 
-#main game loop
-while complete != True:
-	move = movement(current)
-	if move == "food":
-		got_food = food(got_food)
-		current = move
-	elif move == "cabin":
-		cabin(started)
-		current = move
-	elif move == "key":
-		guard_approached = guard(guard_approached)
-		current = move
-	elif move == "hatch":
-		complete = hatch(hatch_opened)
-		current = move
-	else:
-		forest_scenario()
-		current = move
+grab_items = {
+"cabin": "flashlight",
+"clear": "rock",
+}
 
-#Finish message
-print('''
-   _____                                  _           _         _    _                    
-  / ____|                                | |         | |       | |  (_)                   
- | |      ___   _ __    __ _  _ __  __ _ | |_  _   _ | |  __ _ | |_  _   ___   _ __   ___ 
- | |     / _ \ | '_ \  / _` || '__|/ _` || __|| | | || | / _` || __|| | / _ \ | '_ \ / __|
- | |____| (_) || | | || (_| || |  | (_| || |_ | |_| || || (_| || |_ | || (_) || | | |\__ \ 
-  \_____|\___/ |_| |_| \__, ||_|   \__,_| \__| \__,_||_| \__,_| \__||_| \___/ |_| |_||___/
-                        __/ |                                                             
-                       |___/                                                              
-''')
-print('''
-		 __   __ ___   _   _  __        __ ___   _   _   _ 
-		 \ \ / // _ \ | | | | \ \      / // _ \ | \ | | | |
-		  \ V /| | | || | | |  \ \ /\ / /| | | ||  \| | | |
-		   | | | |_| || |_| |   \ V  V / | |_| || |\  | |_|
-		   |_|  \___/  \___/     \_/\_/   \___/ |_| \_| (_)
-                                                   
-''')
+use_items = {
+"hatch": "key",
+"dark": "flashlight",
+"deer": "rock",
+"dog": "deer meat",
+"key": "dog"
+}
+
+class Player():
+	'''A player object'''
+	def __init__(self):
+		self.__location = "cabin"
+		self.inventory = []
+		self.visited = False
+		name = input("What is your name?\n")
+		print(f"Good luck {name},")
+		input(start)
+		print(descriptions[self.__location])
+		print(f"There is a {grab_items[self.__location]} here.")
+
+	@property
+	def location(self):
+		return self.__location
+
+	@location.setter
+	def location(self, new_location):
+		self.__location = new_location
+
+	def dark(self):
+		if not self.visited:
+			self.visited = True
+			if "flashlight" in self.inventory:
+				self.use(self.__location)
+			else:
+				print("You get lost and find yourself at an abandoned campsite.")
+				self.check_item("camp")
+				self.__location = "camp"
+		return self.location
+
+	def movement(self, current):
+		num = 0
+		choice_list = []
+		for i in locations[current]:
+			moves = locations.get(current)[num][0]
+			choice_list.append(moves)
+			num += 1
+		move = self.check_choice(choice_list)
+		for i in locations[current]:
+			if i[0] == move:
+				new_location = i[1]
+				self.__location = new_location
+				print(descriptions[new_location])
+				return new_location
+
+	def check_choice(self, choices):
+		print('\nOptions:')
+		num = 0
+		for i in choices:
+			print(f"- {choices[num].title()}")
+			num += 1
+		choice = input('\n').lower().strip()
+		while choice not in choices:
+			print('\nInvalid command\nOptions:')
+			num = 0
+			for i in choices:
+				print(f"- {choices[num].title()}")
+				num += 1
+			choice = input('').lower().strip()
+		return choice
+	
+	def check_item(self, current):
+		try:
+			if grab_items[current]:
+				print(f"There is a {grab_items[current]} here.")
+		except:
+			pass
+
+	def grab(self, current):
+		try:
+			if grab_items[current]:
+				self.inventory.append(grab_items[current])
+			print(f"You pick up {grab_items[current]}.\n")
+			del grab_items[current]
+			print("Inventory:")
+			for i in self.inventory:
+				print(f"- {i.title()}")
+		except:
+			print("There is nothing here to pick up.")
+
+	def use(self, current):
+		if self.inventory != []:
+			try:
+				print("\nWhat would you like to use?")
+				choice = self.check_choice(self.inventory)
+				if choice == use_items[current]:
+					self.inventory.remove(use_items[current])
+					print(f"You used {use_items[current]}.")
+					if current == "deer":
+						print("You grab some meat from the deer.\n")
+						self.inventory.append("deer meat")
+					if current == "dog":
+						print("You give the deer meat to the dog. He decides to be your pet.\n")
+						self.inventory.append("dog")
+					if current == "hatch":
+						print("You use the key to unlock the hatch. You fall down to your death.\nCongratulations, you have left the simulation\n")
+						win = True
+						return win
+					if current == "key":
+						print("You use your loyal dog to grab the key.\n")
+						self.inventory.append("key")
+					if self.inventory != []:
+						print("Inventory:")
+						for i in self.inventory:
+							print(f"- {i.title()}")
+					else:
+						print("Inventory is now empty.")
+				else:
+					print(f"You cannot use {choice}.")
+			except:
+				print("You cannot use that here.")
+		else:
+			print("Inventory is empty.")
+
+def save(player):
+	with open("save_game.txt", "wb") as file:
+		pickle.dump(player, file)
+		print("Game Saved!")
+
+def load():
+	with open("save_game.txt", "rb") as file:
+		player = pickle.load(file)
+		print("Game Loaded!")
+		print(descriptions[player.location])
+	return player
+
+def quit():
+	print("Thanks for playing!")
+
+player = Player()
+choice = None
+while choice != "quit":
+	choice = player.check_choice(["quit", "move", "save", "load", "grab", "use"])
+	if choice == "quit":
+		quit()
+	elif choice == "move":
+		player.location = player.movement(player.location)
+		player.check_item(player.location)
+		if player.location == "dark":
+			player.dark()
+	elif choice == "save":
+		save(player)
+	elif choice == "load":
+		player = load()
+	elif choice == "grab":
+		player.grab(player.location)
+	elif choice == "use":
+		win = player.use(player.location)
+		if win == True:
+			break
